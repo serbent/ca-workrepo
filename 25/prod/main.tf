e8d3c193f1c6af7fec7b532b6e534f4995a202af
Andrius Solovej <andrius.solovej@gmail.com>
2024-10-24 20:16:31 +0300

    Beutify

--- 25/prod/main.tf
+++ 25/prod/main.tf
@@ -5,11 +5,11 @@ variable environment {
 
 
 module prod {
-    source = "../modules"
-    region = "northeurope"
-    resource_group_name = "${var.environment}-rsgp"
-    virtual_network_name = "${var.environment}-vpc"
-    subnet_name = "${var.environment}-subnet"
-    vm_name =  "${var.environment}-catest001"
-    machine_size = "Standard_B1s"
+    source                  = "../modules"
+    region                  = "northeurope"
+    resource_group_name     = "${var.environment}-rsgp"
+    virtual_network_name    = "${var.environment}-vpc"
+    subnet_name             = "${var.environment}-subnet"
+    vm_name                 = "${var.environment}-catest001"
+    machine_size            = "Standard_B1s"
 }
