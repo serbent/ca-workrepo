77a5b963757388184bca078becb64926850525c1
Andrius Solovej <andrius.solovej@gmail.com>
2024-10-28 19:34:40 +0200

    Declare variable

--- 27/variables.tf
+++ 27/variables.tf
@@ -37,3 +37,9 @@ variable machine_size {
     error_message = "Value can contain only Standard_B1ls, Standard_B1ms, Standard_B1s"
   }
 }
+
+variable public_key {
+  type        = string
+  default     = ""
+  description = "Public key for new VMs"
+}
