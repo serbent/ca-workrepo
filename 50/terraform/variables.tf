3557add77e2f960db2094718c102123ba4c177d9
Andrius Solovej <andrius.solovej@gmail.com>
2025-01-06 21:53:53 +0200

    Tweek for ci/cd

--- 50/terraform/variables.tf
+++ 50/terraform/variables.tf
@@ -3,16 +3,6 @@ variable "aws_region" {
   default     = "eu-central-1"
 }
 
-variable "db_username" {
-  description = "Database username"
-  default     = "admin"
-#   sensitive   = true
-}
-
-variable "db_password" {
-  description = "Database password"
-#   sensitive   = true
-}
 
 variable "environment" {
   description = "Environment"
